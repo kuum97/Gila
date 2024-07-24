cd27f7e988d9c7ce805e246fcc048e5baf4f8dab
juan0444 <dhwjs5267@gmail.com>
2024-07-24 10:00:31 +0900

    feat: 캐러셀 동작, 이미지 구현-데이터 변경 필요

---
+++ components/ui/carousel-card.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import * as React from 'react';
+import Link from 'next/link';
+// import Image from 'next/image';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Autoplay from 'embla-carousel-autoplay';
+import { Card, CardContent } from '@/components/ui/card';
+import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
+import CarouselText from '@/components/ui/carousel-text';
+// import { ActivityItem } from '@/type/activities';
+
+// interface Props {
+//   activities: ActivityItem[];
+// }
+
+// eslint-disable-next-line import/prefer-default-export
+export function CarouselCard({ activities }) {
+  return (
+    <Carousel
+      plugins={[
+        Autoplay({
+          delay: 3000,
+          stopOnInteraction: false,
+          stopOnMouseEnter: true,
+        }),
+      ]}
+    >
+      <CarouselContent>
+        {activities.map(({ id, title, reviewCount }) => (
+          <CarouselItem key={id}>
+            <Card className="rounded-none">
+              <CardContent className="relative p-0 ">
+                <Link href={`/${id}`} passHref>
+                  <div className="w-full h-64">
+                    {/* <Image
+                      src={bannerImageUrl}
+                      alt={title}
+                      fill
+                      className="object-cover w-full h-full"
+                    /> */}
+                  </div>
+                  <CarouselText title={title} reviewCount={reviewCount} />
+                </Link>
+              </CardContent>
+            </Card>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
+  );
+}
