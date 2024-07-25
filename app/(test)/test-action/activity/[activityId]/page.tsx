f9c25e713d87ddacef218fc9c0119e7259d71d1d
daggax <daggax8235@gmail.com>
2024-07-25 23:02:08 +0900

    feat: create sample review 버튼 구현 완료

--- app/(test)/test-action/activity/[activityId]/page.tsx
+++ app/(test)/test-action/activity/[activityId]/page.tsx
@@ -1,5 +1,6 @@
 import { getActivityRequestsByActivityId } from '@/app/data/activity-request';
 import CreateSampleActivityRequestButton from './_components/create-sample-activity-request-button';
+import CreateSampleReviewButton from './_components/create-sample-review-button';
 
 export default async function Page({ params }: { params: { activityId: string } }) {
   const activityId = params.activityId;
@@ -8,15 +9,20 @@ export default async function Page({ params }: { params: { activityId: string } 
   const activityRequests = activityRequestsRes.requests;
   return (
     <div className="space-y-4">
-      <CreateSampleActivityRequestButton activityId={activityId} />
-      <div className="space-y-2">
-        {activityRequests.map((request) => (
-          <div key={request.id} className="bg-slate-300 rounded-md p-2">
-            <div>username: {request.requestUser.nickname}</div>
-            <div>reqId: {request.id}</div>
-            <div>reqStatus: {request.status}</div>
-          </div>
-        ))}
+      <div className="space-y-4">
+        <CreateSampleActivityRequestButton activityId={activityId} />
+        <div className="space-y-2">
+          {activityRequests.map((request) => (
+            <div key={request.id} className="bg-slate-300 rounded-md p-2">
+              <div>username: {request.requestUser.nickname}</div>
+              <div>reqId: {request.id}</div>
+              <div>reqStatus: {request.status}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="space-y-4">
+        <CreateSampleReviewButton activityId={activityId} />
       </div>
     </div>
   );
