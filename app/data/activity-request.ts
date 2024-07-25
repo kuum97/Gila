2b7d4fa96de1661dc44fe205dc96aecdd288f95e
daggax <daggax8235@gmail.com>
2024-07-25 21:41:43 +0900

    feat: activityId 기반으로 가져오는거 안만들어서 추가함.

--- app/data/activity-request.ts
+++ app/data/activity-request.ts
@@ -1,7 +1,7 @@
 import { ActivityRequest } from '@prisma/client';
 import { getCurrentUserId } from '@/app/data/user';
 import { db } from '@/lib/db';
-import { ActivityRequestWithActivity } from '@/type';
+import { ActivityRequestWithActivity, ActivityRequestWithUser } from '@/type';
 
 // 전부 10개
 export const getRegisteredActivities = async (): Promise<{
@@ -55,5 +55,30 @@ export const getRequestedActivities = async (): Promise<{
     return { activities: requestedActivities, cursorId };
   } catch (error) {
     throw new Error('신청한 활동을 가져오는 중에 에러가 발생하였습니다.');
+  }
+};
+
+export const getActivityRequestsByActivityId = async (
+  activityId: string,
+): Promise<{
+  requests: ActivityRequestWithUser[];
+  cursorId: string | null;
+}> => {
+  try {
+    const activityRequests = await db.activityRequest.findMany({
+      where: { activityId },
+      include: { requestUser: true },
+      take: 10,
+      orderBy: {
+        id: 'asc',
+      },
+    });
+
+    const lastActivityRequest = activityRequests[activityRequests.length - 1];
+    const cursorId = lastActivityRequest ? lastActivityRequest.id : null;
+
+    return { requests: activityRequests, cursorId };
+  } catch (error) {
+    throw new Error('활동 요청을 가져오는 중에 에러가 발생하였습니다.');
   }
 };
