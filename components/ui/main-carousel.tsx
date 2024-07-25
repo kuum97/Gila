6bed6e23f93e66e64536f54e17979394d3eebc0f
kuum97 <kuum97@icloud.com>
2024-07-25 22:13:20 +0900

    rename: 파일 위치 변경 및 임포트 경로 수정

--- components/ui/main-carousel.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { CarouselCard } from '@/components/ui/carousel-card';
-
-export default function MainCarousel() {
-  return <CarouselCard activities={data} />;
-}
