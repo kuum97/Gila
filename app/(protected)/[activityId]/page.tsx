cfe801c933779df289fd736ef7db112f24f90748
juan0444 <dhwjs5267@gmail.com>
2024-07-27 12:21:56 +0900

    feat: 상세 페이지 레이아웃 구현

--- app/(protected)/[activityId]/page.tsx
+++ app/(protected)/[activityId]/page.tsx
@@ -1,3 +1,17 @@
-export default async function Page() {
-  return <div>상세 페이지</div>;
+import DetailContent from '@/app/(protected)/[activityId]/_components/detail-content';
+import BottomNav from '@/app/(protected)/[activityId]/_components/bottom-nav';
+
+interface Props {
+  params: { activityId: number };
+}
+
+export default async function Page({ params }: Props) {
+  const id = Number(params.activityId);
+
+  return (
+    <div>
+      <DetailContent />
+      <BottomNav />
+    </div>
+  );
 }
