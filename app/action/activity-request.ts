7ea4d461a69d84fa60e2d0a1c1125dcfa5387f74
daggax <daggax8235@gmail.com>
2024-07-27 11:48:14 +0900

    fix: 기존의 cursor기반으로 가져오는것을 따로 action에 작성하였는데 그냥 get에서 호출과 cursor기반으로 가져오는거 둘다 get에서 처리할려고 작업하였음.

--- app/action/activity-request.ts
+++ app/action/activity-request.ts
@@ -1,7 +1,7 @@
 'use server';
 
 import { db } from '@/lib/db';
-import { ActionType, ActivityRequestWithActivity } from '@/type';
+import { ActionType, RequestWithActivity } from '@/type';
 import { ActivityRequest } from '@prisma/client';
 import { getCurrentUserId } from '../data/user';
 
@@ -86,69 +86,5 @@ export const deleteActivityRequest = async (
     return { success: true, message: '요청 삭제에 성공하였습니다.' };
   } catch (error) {
     return { success: false, message: '요청 삭제 중에 에러가 발생하였습니다.' };
-  }
-};
-
-export const loadMoreRegisteredActivities = async (
-  cursorId: string | null = null,
-): Promise<{ activities: ActivityRequest[]; cursorId: string | null }> => {
-  try {
-    const userId = await getCurrentUserId();
-
-    const registeredActivityRequests = await db.activityRequest.findMany({
-      where: { requestUserId: userId },
-      include: {
-        activity: true,
-      },
-      take: 10,
-      ...(cursorId && {
-        cursor: {
-          id: cursorId,
-        },
-        skip: 1, // cursor 항목 자체를 제외하고 이후 항목부터 가져옴
-      }),
-      orderBy: {
-        id: 'asc',
-      },
-    });
-
-    const lastActivityRequest = registeredActivityRequests[registeredActivityRequests.length - 1];
-    const newCursorId = lastActivityRequest ? lastActivityRequest.id : null;
-
-    return { activities: registeredActivityRequests, cursorId: newCursorId };
-  } catch (error) {
-    throw new Error('활동 요청을 가져오는 중에 에러가 발생하였습니다.');
-  }
-};
-
-export const loadMoreRequestedActivities = async (
-  cursorId: string | null,
-): Promise<{ activities: ActivityRequestWithActivity[]; cursorId: string | null }> => {
-  try {
-    const userId = await getCurrentUserId();
-
-    const requestedActivities = await db.activityRequest.findMany({
-      where: { requestUserId: userId },
-      include: {
-        activity: true,
-      },
-      take: 10,
-      ...(cursorId && {
-        cursor: {
-          id: cursorId,
-        },
-        skip: 1,
-      }),
-      orderBy: {
-        id: 'asc',
-      },
-    });
-
-    const lastActivityRequest = requestedActivities[requestedActivities.length - 1];
-    const newCursorId = lastActivityRequest ? lastActivityRequest.id : null;
-
-    return { activities: requestedActivities, cursorId: newCursorId };
-  } catch (error) {
-    throw new Error('신청한 활동을 가져오는 중에 에러가 발생하였습니다.');
   }
 };
