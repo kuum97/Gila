9259b510381b4a535028f574ded6836bb41cc32a
juan0444 <dhwjs5267@gmail.com>
2024-07-27 10:47:40 +0900

    feat: 상세 페이지 설명, 지도 구현

---
+++ app/(protected)/[activityId]/_components/detail-description.tsx
@@ -0,0 +1,16 @@
+import KakaoMap from '@/app/(protected)/[activityId]/_components/kakao-map';
+import { MapPin } from 'lucide-react';
+
+export default function DetailDescription({ description, locations }) {
+  return (
+    <main>
+      <p className="text-base font-bold text-gray-900 leading-26">체험 설명</p>
+      <p className="mt-1 text-base font-normal text-gray-800 leading-26">{description}</p>
+      <div className="flex mt-8">
+        <MapPin size={20} />
+        <p className="ml-1 text-sm font-normal leading-normal">{locations}</p>
+      </div>
+      <KakaoMap address={locations} />
+    </main>
+  );
+}
