eb89a560c10c0fc9789f32c2a40ce9656b4a498a
Batrnan <tjdwo2323@naver.com>
2024-07-29 21:19:12 +0900

    feat: 로고 랭크 파일 생성

---
+++ app/(protected)/(user)/profile/_components/profile-rank.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+import React from 'react';
+
+export default function ProfileRank() {
+  return (
+    <div>
+      <Image src="/logoimg.svg" alt="icon" />
+    </div>
+  );
+}
