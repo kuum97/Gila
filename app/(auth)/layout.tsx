dd53093595f626c613a3c855da4544d91b70c48c
kuum97 <kuum97@icloud.com>
2024-07-29 14:03:31 +0900

    style: pr 피드백 적용

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen px-8 overflow-hidden bg-white">
-      <Link href="/">
-        <Image width={250} height={250} src="/GilaName.png" alt="logo-to-home" />
+      <Link href="/" className="mb-2">
+        <Image width={200} height={200} src="/GilaLogo.png" alt="logo-to-home" />
       </Link>
       <main className="w-[450px] px-10">{children}</main>
     </div>
