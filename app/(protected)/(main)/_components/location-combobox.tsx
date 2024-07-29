34b9f44538ad85175e81dd7104f04eb27cdd2bd3
kuum97 <kuum97@icloud.com>
2024-07-29 12:55:47 +0900

    style: 지역 선택 콤보박스 스타일링을 위해 Command.tsx수정 및 스타일 적용

--- app/(protected)/(main)/_components/location-combobox.tsx
+++ app/(protected)/(main)/_components/location-combobox.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import * as React from 'react';
-import { Check } from 'lucide-react';
-import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 import {
   Command,
@@ -14,6 +12,7 @@ import {
 } from '@/components/ui/command';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
 import { LOCATIONS } from '@/constants/locations';
+import { RefreshCcw } from 'lucide-react';
 
 export default function LocationCombobox() {
   const [open, setOpen] = React.useState<boolean>(false);
@@ -25,7 +24,7 @@ export default function LocationCombobox() {
       setLocation(currentValue === location ? '' : currentValue);
     },
     [location],
-  ); // 현재 대분류 상태 설정
+  ); // 현재 도, 광역시, 특별시 결정
 
   const handleSelectSubLocation = React.useCallback(
     (currentValue: string) => {
@@ -33,7 +32,7 @@ export default function LocationCombobox() {
       setOpen(false);
     },
     [subLocation],
-  );
+  ); // 세부 행정구역 결정
 
   const subLocations = React.useMemo(() => {
     return location ? LOCATIONS[location] : [];
@@ -59,51 +58,51 @@ export default function LocationCombobox() {
         <Command>
           <CommandInput placeholder="목적지를 찾아보세요!" />
           <CommandEmpty>그런 지역은 없어요</CommandEmpty>
-          <Button
-            type="button"
-            onClick={() => {
-              setLocation('');
-              setSubLocation('');
-            }}
-            className="text-white bg-black rounded-none"
-          >
-            초기화
-          </Button>
-          <CommandList className="flex">
+          <CommandList className="flex justify-center w-[350x] p-1 relative">
             {location || (
-              <CommandGroup heading="대분류" className="flex">
-                {Object.keys(LOCATIONS).map((province) => (
-                  <CommandItem
-                    key={province}
-                    value={province}
-                    onSelect={handleSelectValue}
-                    className="flex hover:bg-gray-100"
-                  >
-                    {province}
-                  </CommandItem>
-                ))}
+              <CommandGroup heading="도・광역시・특별시">
+                <ul className="grid grid-cols-3 gap-3 mt-1">
+                  {Object.keys(LOCATIONS).map((province) => (
+                    <CommandItem
+                      key={province}
+                      value={province}
+                      onSelect={handleSelectValue}
+                      className="flex items-center justify-center w-full border rounded-lg hover:bg-gray-100"
+                    >
+                      {province}
+                    </CommandItem>
+                  ))}
+                </ul>
               </CommandGroup>
             )}
             {location && (
-              <CommandGroup heading="소분류" className="flex">
-                {subLocations.map((city) => (
-                  <CommandItem
-                    key={city}
-                    value={city}
-                    onSelect={handleSelectSubLocation}
-                    className="flex hover:bg-gray-100"
-                  >
-                    <Check
-                      className={cn(
-                        'mr-2 h-4 w-4',
-                        city === subLocation ? 'opacity-100' : 'opacity-0',
-                      )}
-                    />
-                    {city}
-                  </CommandItem>
-                ))}
+              <CommandGroup>
+                <ul className="grid grid-cols-3 gap-3 mt-1">
+                  {subLocations.map((city) => (
+                    <CommandItem
+                      key={city}
+                      value={city}
+                      onSelect={handleSelectSubLocation}
+                      className="flex items-center justify-center w-full border rounded-lg hover:bg-gray-100"
+                    >
+                      {city}
+                    </CommandItem>
+                  ))}
+                </ul>
               </CommandGroup>
             )}
+            <div className="absolute flex justify-end right-3 top-1.5">
+              <Button
+                type="button"
+                onClick={() => {
+                  setLocation('');
+                  setSubLocation('');
+                }}
+                className="flex items-center justify-center p-1 text-white bg-black rounded-full size-6"
+              >
+                <RefreshCcw className="size-full" />
+              </Button>
+            </div>
           </CommandList>
         </Command>
       </PopoverContent>
