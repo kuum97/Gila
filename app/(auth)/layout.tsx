b0ade3f521cfa334629880e661d70c0bd2568d85
kuum97 <kuum97@icloud.com>
2024-07-29 14:05:54 +0900

    style: 로고 크기 수정

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen px-8 overflow-hidden bg-white">
       <Link href="/" className="mb-2">
-        <Image width={200} height={200} src="/GilaLogo.png" alt="logo-to-home" />
+        <Image width={180} height={180} src="/GilaLogo.png" alt="logo-to-home" />
       </Link>
       <main className="w-[450px] px-10">{children}</main>
     </div>
