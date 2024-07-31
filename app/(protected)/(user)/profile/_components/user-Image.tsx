505cfe66d5c418027fc8f91cb99410911f4c5091
juan0444 <dhwjs5267@gmail.com>
2024-07-31 11:51:45 +0900

    feat: 프로필 이미지 추가

---
+++ app/(protected)/(user)/profile/_components/user-Image.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import { Input } from '@/components/ui/input';
+import Label from '@/components/ui/label';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+import { useRef, useState } from 'react';
+import { Pencil } from 'lucide-react';
+
+export default function UserImage({ edit = false }) {
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
+  const [imageFile, setImageFile] = useState<string | null>(null);
+
+  const fileInputRef = useRef(null);
+
+  // const handleIconClick = () => {
+  //   fileInputRef.current.click();
+  // };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files?.[0]) {
+      const file = e.target.files[0];
+      setImageFile(file);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <div>
+      <div className="flex flex-col items-center">
+        <div className="relative w-32 h-32">
+          <Avatar className="w-full h-full">
+            <AvatarImage
+              src={imagePreview || '/test.png'}
+              alt="Profile"
+              className="object-cover w-full h-full border border-gray-300 rounded-full"
+            />
+            <AvatarFallback>U</AvatarFallback>
+          </Avatar>
+          {edit && (
+            // <div className="relative">
+            //   <div
+            //     className="absolute bottom-0 right-0 p-2 bg-white border border-gray-300 rounded-full shadow-lg cursor-pointer"
+            //     onClick={handleIconClick}
+            //   >
+            //     <Pencil className="text-gray-600" size={20} />
+            //   </div>
+            //   <div className="hidden">
+            //     <Label htmlFor="picture" className="sr-only">
+            //       Picture
+            //     </Label>
+            //     <Input id="picture" type="file" ref={fileInputRef} onChange={handleFileChange} />
+            //   </div>
+            // </div>
+
+            <div className="relative">
+              <Label
+                htmlFor="picture"
+                className="absolute bottom-0 right-0 p-2 bg-white border border-gray-300 rounded-full shadow-lg cursor-pointer"
+              >
+                <Pencil className="text-gray-600" size={20} />
+              </Label>
+              <Input id="picture" type="file" className="hidden" onChange={handleFileChange} />
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
