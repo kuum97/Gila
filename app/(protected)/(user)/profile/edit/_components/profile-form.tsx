4cfdd618c6fc55176e748ea9bcae854777c2fb32
juan0444 <dhwjs5267@gmail.com>
2024-07-31 11:54:46 +0900

    feat: 내 정보 수정 form 추가

--- app/(protected)/(user)/profile/edit/_components/profile-form.tsx
+++ app/(protected)/(user)/profile/edit/_components/profile-form.tsx
@@ -1,13 +1,56 @@
-import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { Button } from '@/components/ui/button';
 
-export default function ProfileForm() {
+export default function ProfileForm({
+  schema,
+  defaultValues,
+  name,
+  label,
+  placeholder,
+  type,
+  onSubmit,
+}) {
+  const form = useForm({
+    resolver: zodResolver(schema),
+    defaultValues,
+  });
+
   return (
-    <div>
-      <div className="flex items-center w-full max-w-sm space-x-2">
-        <Input type="email" placeholder="Email" />
-        <Button type="submit">Subscribe</Button>
-      </div>
-    </div>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex items-center justify-center gap-6 mx-2"
+      >
+        <div className="flex flex-col w-full">
+          <FormField
+            control={form.control}
+            name={name}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="hidden text-sm">{label}</FormLabel>
+                <FormControl>
+                  <Input type={type} placeholder={placeholder} {...field} className="text-xs" />
+                </FormControl>
+                <FormMessage className="text-xs text-red" />
+              </FormItem>
+            )}
+          />
+        </div>
+        <Button type="submit" className="mt-4 text-sm">
+          저장하기
+        </Button>
+      </form>
+    </Form>
   );
 }
