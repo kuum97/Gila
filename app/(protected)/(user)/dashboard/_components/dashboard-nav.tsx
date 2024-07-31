f1d60fd00611e67605c1cfe4934dc83986be08d9
Batrnan <tjdwo2323@naver.com>
2024-07-31 16:05:14 +0900

    feat: dashboard nav 구현

---
+++ app/(protected)/(user)/dashboard/_components/dashboard-nav.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { cn } from '@/lib/utils';
+import { CheckCircle, Clipboard, Edit, Heart, HelpCircle, Star, LucideIcon } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+interface Route {
+  icon: LucideIcon;
+  label: string;
+  href: string;
+}
+
+const routes: Route[] = [
+  { icon: Edit, label: '등록한 활동', href: '/dashboard/my-activity' },
+  { icon: Clipboard, label: '신청한 활동', href: '/dashboard/promise-list' },
+  { icon: CheckCircle, label: '신청 현황', href: '/dashboard/promised-list' },
+  { icon: Heart, label: '찜 목록', href: '/dashboard/wishlist' },
+  { icon: HelpCircle, label: '내 질문', href: '/dashboard/my-question' },
+  { icon: Star, label: '리뷰', href: '/dashboard/reviews' },
+];
+
+export default function DashboardNav() {
+  const pathname = usePathname();
+
+  return (
+    <div className="overflow-x-auto whitespace-nowrap border-b border-gray-300 bg-white hide-scrollbar">
+      <div className="flex">
+        {routes.map((route) => {
+          const isActive = pathname === route.href;
+          return (
+            <Link key={route.label} href={route.href}>
+              <div
+                className={cn(
+                  'flex flex-col items-center gap-2 text-xs p-3 w-20',
+                  isActive && 'border-b border-primary text-primary',
+                )}
+              >
+                <route.icon size={20} />
+                <span>{route.label}</span>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
--- app/(protected)/(user)/dashboard/layout.tsx
+++ app/(protected)/(user)/dashboard/layout.tsx
@@ -1,5 +1,11 @@
 import React, { ReactNode } from 'react';
+import DashboardNav from './_components/dashboard-nav';
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <div className="p-5">{children}</div>;
+  return (
+    <div>
+      <DashboardNav />
+      <div className="p-5">{children}</div>
+    </div>
+  );
 }
