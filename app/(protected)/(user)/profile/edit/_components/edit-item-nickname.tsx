02a45d9b4f38ebc1ee9042d042eb081479814666
juan0444 <dhwjs5267@gmail.com>
2024-07-31 20:14:03 +0900

    refactor: 닉네임과 비밀번호로 item 컴포넌트를 분리해서 구현

---
+++ app/(protected)/(user)/profile/edit/_components/edit-item-nickname.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
+import { useState } from 'react';
+import EditNicknameForm from './edit-nickname-form';
+
+interface Props {
+  value: string;
+  triggerText: string;
+}
+
+export default function EditItemNickname({ value, triggerText }: Props) {
+  const [editNickname, setEditNickname] = useState('닉네임 테스트');
+
+  return (
+    <Accordion type="single" collapsible className="flex flex-col gap-8">
+      <AccordionItem value={value}>
+        <AccordionTrigger>
+          <p className="w-20 text-sm text-black cursor-pointer">{triggerText}</p>
+          <div className="flex items-center justify-between w-full mx-3">
+            <p className="text-sm font-bold">{editNickname}</p>
+            <span className="text-xs text-gray-400 cursor-pointer">수정하기</span>
+          </div>
+        </AccordionTrigger>
+        <AccordionContent>
+          <EditNicknameForm setValue={setEditNickname} />
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  );
+}
