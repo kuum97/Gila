75f26e3c39104c2093a282c252928f5f2ea27a25
kuum97 <kuum97@icloud.com>
2024-08-02 00:10:49 +0900

    feat: 활동 리스트와 개별 카드 구현

--- app/(protected)/(main)/activity-list/_components/activity-list-card.tsx
+++ app/(protected)/(main)/activity-list/_components/activity-list-card.tsx
@@ -1,28 +1,55 @@
 import Image from 'next/image';
 import React from 'react';
 import { Heart } from 'lucide-react';
+import { getUserProfileWithIntroducedInfos } from '@/app/data/user';
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import Link from 'next/link';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { ActivityWithUserAndFavoCount } from '@/type';
 
-export default function ActivityListCard() {
+interface Props {
+  activity: ActivityWithUserAndFavoCount;
+}
+
+export default async function ActivityListCard({ activity }: Props) {
+  const { title, startDate, endDate, location, views, userId, _count } = activity;
+  const owner = await getUserProfileWithIntroducedInfos(userId);
+
   return (
-    <div className="w-full border p-3 rounded-lg flex gap-6">
-      <div className="w-[130px] h-[130px] relative rounded-lg overflow-hidden flex-shrink-0">
-        <Image src="/test.png" alt="썸네일" layout="fill" objectFit="cover" />
-      </div>
-      <div className="flex flex-col justify-center gap-2 w-full overflow-hidden">
-        <h1 className="text-sm font-bold truncate w-full">함께 배우는 즐거운 스트릿 댄스</h1>
-        <div className="text-xs flex flex-col gap-[1px]">
-          <p className="font-bold">페이커</p>
-          <p>2024-07-19 ~ 2024-07-19</p>
-          <p>16 : 00 ~ 18 : 00</p>
-          <p>서울 중구 청계천로</p>
+    <Card>
+      <Link
+        href={`/${activity.id}`}
+        className="flex w-full justify-center gap-3 p-1 border rounded-lg shadow-md bg-[#ffffff]"
+      >
+        <div className="w-[130px] h-[130px] relative rounded-lg overflow-hidden flex-shrink-0">
+          <Image src="/test.png" alt="thumbnail" fill style={{ objectFit: 'cover' }} />
         </div>
-        <div className="text-xs flex gap-2 justify-end">
-          <button className="flex items-center gap-1" type="button">
-            <Heart size={15} />4
-          </button>
-          <p>조회수 14</p>
+        <div className="flex flex-col justify-between w-full pt-1 overflow-hidden">
+          <CardHeader className="p-0">
+            <CardTitle className="w-full text-xl font-bold truncate">{title}</CardTitle>
+          </CardHeader>
+          <CardContent className="text-sm gap-1 flex flex-col p-0">
+            <div className="flex items-center">
+              <Avatar className="size-6 mr-2">
+                <AvatarImage src={owner.user.image || '/test.png'} />
+                <AvatarFallback>G</AvatarFallback>
+              </Avatar>
+              <span className="font-medium text-black">{owner.user.nickname}</span>
+            </div>
+            <span className="text-xs text-gray-500">
+              {startDate.getDate().toString()} ~ {endDate.getDate().toString()}
+            </span>
+            <span className="text-xs text-gray-500">{location}</span>
+          </CardContent>
+          <CardFooter className="flex p-0 pr-2 justify-end gap-2 text-xs">
+            <span className="text-xs text-gray-500">조회수 {views}</span>
+            <button className="flex items-center gap-1 text-xs text-gray-500" type="button">
+              <Heart className="text-red" size={15} />
+              {_count.favorites}
+            </button>
+          </CardFooter>
         </div>
-      </div>
-    </div>
+      </Link>
+    </Card>
   );
 }
--- app/(protected)/(main)/activity-list/_components/activity-list.tsx
+++ app/(protected)/(main)/activity-list/_components/activity-list.tsx
@@ -1,3 +1,18 @@
-export default function ActivityList() {
-  return <ul>list</ul>;
+import ActivityListCard from '@/app/(protected)/(main)/activity-list/_components/activity-list-card';
+import { ActivityWithUserAndFavoCount } from '@/type';
+
+interface Props {
+  activities: ActivityWithUserAndFavoCount[];
+}
+
+export default function ActivityList({ activities }: Props) {
+  return (
+    <ul className="flex flex-col gap-2">
+      {activities.map((activity) => (
+        <li key={activity.id}>
+          <ActivityListCard activity={activity} />
+        </li>
+      ))}
+    </ul>
+  );
 }
