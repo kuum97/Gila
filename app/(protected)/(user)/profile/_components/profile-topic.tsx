139166c0f394dad7ecbb01e520d9e61c71d01668
juan0444 <dhwjs5267@gmail.com>
2024-08-01 17:58:00 +0900

    feat: 빈배열인 경우 추가

--- app/(protected)/(user)/profile/_components/profile-topic.tsx
+++ app/(protected)/(user)/profile/_components/profile-topic.tsx
@@ -6,7 +6,7 @@ interface Props {
   edit?: boolean;
 }
 
-export default function ProfileTopic({ tags, edit = false }: Props) {
+export default function ProfileTopic({ tags = [], edit = false }: Props) {
   const getTagColor = (item: string) => {
     const tagInfo = TAGS.find((tagItem) => tagItem.tag.includes(item));
     return tagInfo ? tagInfo.color : '#FFB800';
@@ -15,18 +15,22 @@ export default function ProfileTopic({ tags, edit = false }: Props) {
   return (
     <div className="flex justify-between w-full pb-4 mx-1 border-b border-gray-200">
       <div className="flex flex-wrap gap-1 w-80">
-        {tags.map((item: string) => (
-          <span
-            key={item}
-            className="flex justify-center items-center px-2 py-1 text-xs font-bold text-black rounded-3xl"
-            style={{ backgroundColor: getTagColor(item) }}
-          >
-            {item}
-          </span>
-        ))}
+        {tags.length === 0 ? (
+          <p className="text-sm text-gray-400">토픽이 없습니다.</p>
+        ) : (
+          tags.map((item: string) => (
+            <span
+              key={item}
+              className="flex items-center justify-center px-2 py-1 text-xs font-bold text-black rounded-3xl"
+              style={{ backgroundColor: getTagColor(item) }}
+            >
+              {item}
+            </span>
+          ))
+        )}
       </div>
       {edit && (
-        <Link href="/topic" className="mr-6 text-xs text-gray-400 w-16">
+        <Link href="/topic" className="w-16 mr-6 text-xs text-gray-400">
           수정하기
         </Link>
       )}
