3d773a15e292fc3bd835e041935ac2c19953b8af
juan0444 <dhwjs5267@gmail.com>
2024-08-01 17:57:02 +0900

    feat: 실제 데이터로 변경

--- app/(protected)/(user)/profile/_components/profile-Item.tsx
+++ app/(protected)/(user)/profile/_components/profile-Item.tsx
@@ -2,25 +2,28 @@
 
 import ProfileTopic from '@/app/(protected)/(user)/profile/_components/profile-topic';
 
-export default function ProfileItem() {
+export default function ProfileItem({ userData }) {
+  const { user, activityCount, questionCount } = userData;
+  const { nickname, tags } = user;
+
   return (
     <div className="flex flex-col gap-8 mt-12">
       <div className="flex w-full gap-4 pb-4 border-b border-gray-200 mx-1flex">
         <p className="text-sm">닉네임</p>
-        <p className="text-sm font-bold">닉네임 테스트</p>
+        <p className="text-sm font-bold">{nickname}</p>
       </div>
-      <ProfileTopic tags={['내향', '계획적', '홀로', '액티비티']} />
+      <ProfileTopic tags={tags} />
       <div className="w-full pb-4 border-b border-gray-200">
         <p className="text-sm">점수</p>
       </div>
       <div className="flex w-full pb-4 border-b border-gray-200 justify-evenly">
         <div className="text-center">
           <p className="text-sm">가이드 횟수</p>
-          <p className="text-sm font-bold">10</p>
+          <p className="text-sm font-bold">{activityCount}</p>
         </div>
         <div className="text-center">
           <p className="text-sm">질문 횟수</p>
-          <p className="text-sm font-bold">10</p>
+          <p className="text-sm font-bold">{questionCount}</p>
         </div>
       </div>
     </div>
--- app/(protected)/(user)/profile/edit/_components/edit-profile-item.tsx
+++ app/(protected)/(user)/profile/edit/_components/edit-profile-item.tsx
@@ -3,19 +3,26 @@
 import ProfileTopic from '@/app/(protected)/(user)/profile/_components/profile-topic';
 import EditItemNickname from '@/app/(protected)/(user)/profile/edit/_components/edit-item-nickname';
 import EditItemPassword from '@/app/(protected)/(user)/profile/edit/_components/edit-item-password';
+import { User } from '@/type';
 
-export default function EditProfileItem() {
+interface Props {
+  user: User;
+}
+
+export default function EditProfileItem({ user }) {
+  const { email, nickname, password, tags } = user;
+
   return (
     <div className="flex flex-col gap-8 mt-12">
       <div className="flex w-full gap-4 pb-4 mx-1 border-b border-gray-200">
         <p className="text-sm">이메일</p>
-        <p className="text-sm font-bold">test@test.com</p>
+        <p className="text-sm font-bold">{email}</p>
       </div>
       <div className="flex flex-col gap-8">
-        <EditItemNickname value="nickname" triggerText="닉네임" />
-        <EditItemPassword value="password" triggerText="비밀번호" />
+        <EditItemNickname value={nickname} triggerText="닉네임" />
+        <EditItemPassword value={password} triggerText="비밀번호" />
       </div>
-      <ProfileTopic tags={['내향', '계획적', '홀로', '액티비티']} edit />
+      <ProfileTopic tags={tags} edit />
     </div>
   );
 }
