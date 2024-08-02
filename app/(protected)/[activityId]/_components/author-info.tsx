e5bcb9e8a2a861b85a42850f31860d775ece8ab3
sj0724 <sj07245@naver.com>
2024-08-02 12:32:48 +0900

    fix: 등록한 유저 클릭시 소개 페이지 이동 및 캐러셀 수정

--- app/(protected)/[activityId]/_components/author-info.tsx
+++ app/(protected)/[activityId]/_components/author-info.tsx
@@ -1,5 +1,6 @@
 import { getUserProfileWithIntroducedInfos } from '@/app/data/user';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import Link from 'next/link';
 
 interface Props {
   ownerId: string;
@@ -9,23 +10,25 @@ export default async function AuthorInfo({ ownerId }: Props) {
   const owner = await getUserProfileWithIntroducedInfos(ownerId);
 
   return (
-    <div>
+    <div className="flex flex-col justify-start gap-2">
       <p className="text-sm font-bold">등록한 길라</p>
-      <div className="flex items-center gap-4 p-4 mt-1 mb-32 border border-solid rounded-lg">
-        <div className="flex items-center gap-1 text-xs">
-          <Avatar>
-            <AvatarImage
-              src={owner.user.image ? owner.user.image : '/test.png'}
-              className="object-cover w-10 h-10 rounded-full"
-            />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
+      <Link href={`/introduction?userId=${ownerId}`} className="h-fit">
+        <div className="flex items-center gap-4 p-4 mt-1 mb-28 border border-solid rounded-lg">
+          <div className="flex items-center gap-1 text-xs">
+            <Avatar>
+              <AvatarImage
+                src={owner.user.image ? owner.user.image : '/test.png'}
+                className="object-cover w-10 h-10 rounded-full"
+              />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </div>
+          <div className="flex flex-col justify-between">
+            <p className="text-sm">{owner.user.nickname}</p>
+            <p className="text-sm">{owner.averageReviewScore}</p>
+          </div>
         </div>
-        <div className="flex flex-col justify-between">
-          <p className="text-sm">{owner.user.nickname}</p>
-          <p className="text-sm">{owner.averageReviewScore}</p>
-        </div>
-      </div>
+      </Link>
     </div>
   );
 }
--- app/(protected)/[activityId]/_components/detail-carousel.tsx
+++ app/(protected)/[activityId]/_components/detail-carousel.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import Autoplay from 'embla-carousel-autoplay';
 import { Card, CardContent } from '@/components/ui/card';
 import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
-import { Dialog, DialogContent } from '@/components/ui/dialog';
+import { Dialog, DialogContent, DialogDescription, DialogTitle } from '@/components/ui/dialog';
 
 // eslint-disable-next-line import/prefer-default-export
 export function DetailCarousel({ thumbnails }: { thumbnails: string[] }) {
@@ -35,7 +35,7 @@ export function DetailCarousel({ thumbnails }: { thumbnails: string[] }) {
             <CarouselItem key={imageUrl}>
               <Card onClick={() => handleImageClick(imageUrl)}>
                 <CardContent className="relative p-0">
-                  <div className="w-full h-60">
+                  <div className="w-full h-60 relative">
                     <Image src={imageUrl} alt="캐러셀 이미지" fill className="object-cover" />
                   </div>
                 </CardContent>
@@ -47,8 +47,10 @@ export function DetailCarousel({ thumbnails }: { thumbnails: string[] }) {
 
       {selectedImage && (
         <Dialog open={!!selectedImage} onOpenChange={handleClose}>
-          <DialogContent className="z-50 w-full h-auto max-w-md p-4 mx-auto my-8">
-            <div className="w-full h-full max-w-full max-h-full">
+          <DialogContent className="z-50 w-full h-auto max-w-md p-4 mx-auto my-8 border-none">
+            <DialogTitle aria-describedby={undefined} />
+            <DialogDescription aria-describedby={undefined} />
+            <div className="w-full h-full max-w-full max-h-full relative">
               <Image
                 src={selectedImage}
                 alt="원본 이미지"
