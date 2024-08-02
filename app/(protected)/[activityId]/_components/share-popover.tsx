e8e47871a540c0862c96f86ee34c69b86e11a68e
sj0724 <sj07245@naver.com>
2024-08-02 12:12:48 +0900

    feat: kakao공유하기 기능 추가

--- app/(protected)/[activityId]/_components/share-popover.tsx
+++ app/(protected)/[activityId]/_components/share-popover.tsx
@@ -4,11 +4,34 @@ import { ExternalLink, Link, MessageCircle } from 'lucide-react';
 import { toast } from 'sonner';
 
 export default function SharePopover({ activityId }: { activityId: string }) {
-  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/sharedFolder/${activityId}`;
+  const url = `${process.env.NEXT_PUBLIC_BASE_URL}/${activityId}`;
 
   const copyLink = async () => {
     await navigator.clipboard.writeText(url);
     toast.message('링크를 클립보드에 복사했습니다.');
+  };
+
+  const shareKakao = () => {
+    const { Kakao } = window;
+    Kakao.Share.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: 'Gila',
+        description: '길라와 같이 떠나자',
+        imageUrl: '',
+        link: {
+          mobileWebUrl: url,
+        },
+      },
+      buttons: [
+        {
+          title: '새로운 경험하러 가기',
+          link: {
+            mobileWebUrl: url,
+          },
+        },
+      ],
+    });
   };
 
   return (
@@ -23,7 +46,10 @@ export default function SharePopover({ activityId }: { activityId: string }) {
         >
           <Link width={20} />
         </div>
-        <div className="w-8 h-8 rounded-full flex justify-center items-center bg-[#F9E000] shadow-md">
+        <div
+          className="w-8 h-8 rounded-full flex justify-center items-center bg-[#F9E000] shadow-md"
+          onClick={shareKakao}
+        >
           <MessageCircle width={20} fill="#3b1f06" />
         </div>
       </PopoverContent>
