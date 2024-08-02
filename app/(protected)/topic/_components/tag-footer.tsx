ecd719ced3d2f556e657e6469debf635afae07fc
sj0724 <sj07245@naver.com>
2024-08-02 16:19:31 +0900

    feat: 최초 로그인시 토픽 페이지, 버튼 클릭시 최초 로그인 해제

--- app/(protected)/topic/_components/tag-footer.tsx
+++ app/(protected)/topic/_components/tag-footer.tsx
@@ -1,21 +1,21 @@
 import { Button } from '@/components/ui/button';
 import Progress from '@/components/ui/progress';
 import { TAGS } from '@/constants/tag';
-import Link from 'next/link';
 
 interface Props {
   page: number;
   editTag: () => void;
+  passTag: () => void;
 }
 
-export default function TagFooter({ page, editTag }: Props) {
+export default function TagFooter({ page, editTag, passTag }: Props) {
   const processStatus = page * (100 / TAGS.length);
   const lastTagPage = TAGS.length;
 
   return (
     <div className="flex items-center justify-center w-full gap-5 my-5">
-      <Button asChild className="w-20 bg-gray_100">
-        <Link href="/activity-list">건너뛰기</Link>
+      <Button className="w-20 bg-gray_100" onClick={passTag}>
+        건너뛰기
       </Button>
       <Progress value={processStatus} className="w-[40%] bg-gray_100 h-1" />
       <Button className="w-20" disabled={page !== lastTagPage} onClick={editTag}>
