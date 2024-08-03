bdcc88a5c0b214a87514a8b3db6f3807e1deae22
daggax <daggax8235@gmail.com>
2024-08-03 13:01:53 +0900

    feat: review page작성 완료.

--- app/(protected)/(user)/(dashboard)/reviews/_components/reviews-card.tsx
+++ app/(protected)/(user)/(dashboard)/reviews/_components/reviews-card.tsx
@@ -2,45 +2,77 @@
 
 /* eslint-disable no-console */
 
-import React, { useState } from 'react';
+import React, { useState, useTransition } from 'react';
 import UserIcon from '@/components/user-icon';
-import SmallButton from '@/components/small-button';
 import RatingSelector from '@/app/(protected)/(user)/(dashboard)/reviews/_components/rating-selector';
+import { toast } from 'sonner';
+import { Button } from '@/components/ui/button';
+import { createReview } from '@/app/action/review';
 
-export default function ReviewsCard() {
+type Props = {
+  activityId: string;
+  title: string;
+  startDate: string;
+  endDate: string;
+  userImg: string;
+  nickname: string;
+  removeActivity: (activityId: string) => void;
+};
+
+export default function ReviewsCard({
+  activityId,
+  title,
+  startDate,
+  endDate,
+  userImg,
+  nickname,
+  removeActivity,
+}: Props) {
   const [selectedRating, setSelectedRating] = useState<number>(-1);
+  const [isPending, startTransition] = useTransition();
 
   const handleRatingClick = (rating: number) => {
     if (selectedRating === rating) {
-      setSelectedRating(-1); // 동일한 값을 클릭하면 선택 해제
+      setSelectedRating(-1);
     } else {
-      setSelectedRating(rating); // 새로운 값을 클릭하면 선택
+      setSelectedRating(rating);
     }
   };
 
-  // 리뷰 제출 테스트
   const handleSubmit = () => {
     if (selectedRating === -1) {
-      console.log('리뷰를 눌러주세요!');
+      toast.error('리뷰를 눌러주세요!');
       return;
     }
-    console.log(`rating: ${selectedRating}`);
+    startTransition(async () => {
+      const action = await createReview({ activityId, rating: selectedRating });
+      if (!action.success) {
+        toast.error(action.message);
+        return;
+      }
+      toast.success(action.message);
+      removeActivity(activityId);
+    });
   };
 
   return (
     <div className="w-full border p-3 rounded-lg flex gap-6 relative">
       <div className="flex flex-col justify-center gap-2 w-full overflow-hidden">
-        <h1 className="text-sm font-bold truncate w-full">함께 배우는 즐거운 스트릿 댄스</h1>
+        <h1 className="text-sm font-bold truncate w-full">{title}</h1>
         <div className="text-xs flex flex-col gap-2">
           <div className="flex gap-3">
-            <p>2024-07-19 ~ 2024-07-19</p>•<p>16 : 00 ~ 18 : 00</p>
+            <p>
+              {startDate} ~ {endDate}
+            </p>
           </div>
-          <UserIcon name="바보" />
+          <UserIcon imageSrc={userImg} name={nickname} />
         </div>
         <RatingSelector selectedRating={selectedRating} onRatingClick={handleRatingClick} />
       </div>
       <div className="absolute top-3 right-3">
-        <SmallButton name="제출" color="bg-primary" onClick={handleSubmit} />
+        <Button disabled={isPending} onClick={handleSubmit} className="h-auto px-2 py-1">
+          제출
+        </Button>
       </div>
     </div>
   );
