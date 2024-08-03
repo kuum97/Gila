d09ba004f489707aabf4d4d9f5354a49d33cd794
kuum97 <kuum97@icloud.com>
2024-08-03 19:32:35 +0900

    feat: promised 리스트 구현

---
+++ app/(protected)/(user)/(dashboard)/promised-list/_components/promised-list.tsx
@@ -0,0 +1,22 @@
+import { RequestWithReqUser } from '@/type';
+import PromisedListCard from '@/app/(protected)/(user)/(dashboard)/promised-list/_components/promised-list-card';
+
+interface Props {
+  promisedActivities: RequestWithReqUser[];
+}
+
+export default function PromisedList({ promisedActivities }: Props) {
+  return (
+    <ul>
+      {promisedActivities.length > 0 ? (
+        promisedActivities.map((activity) => (
+          <li key={activity.id}>
+            <PromisedListCard requestId={activity.requestUserId} />
+          </li>
+        ))
+      ) : (
+        <div>none</div>
+      )}
+    </ul>
+  );
+}
