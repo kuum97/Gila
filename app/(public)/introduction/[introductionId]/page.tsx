e77a5e0b0c887b786469849d2e4367f426411f1c
juan0444 <dhwjs5267@gmail.com>
2024-08-04 17:39:57 +0900

    fix: 프로필 이미지 수정

--- app/(public)/introduction/[introductionId]/page.tsx
+++ app/(public)/introduction/[introductionId]/page.tsx
@@ -1,5 +1,5 @@
 import ProfileImage from '@/app/(protected)/(user)/profile/_components/profile-image';
-import { getCurrentUser, getUserProfileWithIntroducedInfos } from '@/app/data/user';
+import { getUserProfileWithIntroducedInfos } from '@/app/data/user';
 import ProfileItem from '@/components/profile-item';
 
 interface Params {
@@ -8,12 +8,11 @@ interface Params {
 
 export default async function Page({ params }: { params: Params }) {
   const id = params.introductionId;
-  const { image } = await getCurrentUser();
   const userData = await getUserProfileWithIntroducedInfos(id);
 
   return (
     <div className="m-8">
-      <ProfileImage image={image} />
+      <ProfileImage image={userData.user.image} />
       <ProfileItem userData={userData} />
     </div>
   );
