11ac0a87d8920ee81178040cde679efc15aadfa9
sj0724 <sj07245@naver.com>
2024-08-04 22:56:20 +0900

    fix: dropdown sorting 수정

--- app/(protected)/(main)/question-list/_components/question-sort-dropdown.tsx
+++ app/(protected)/(main)/question-list/_components/question-sort-dropdown.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
@@ -5,9 +7,31 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
-import Link from 'next/link';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { useCallback } from 'react';
 
 export default function QuestionSortDropdown({ sortValue }: { sortValue: string }) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const createQueryString = useCallback(
+    (name: string, value: string) => {
+      const params = new URLSearchParams(searchParams.toString());
+      if (value) {
+        params.set(name, value);
+      } else {
+        params.delete(name);
+      }
+      return params.toString();
+    },
+    [searchParams],
+  );
+
+  const handleSorting = ({ name, value }: { name: string; value: string }) => {
+    router.push(`${pathname}?${createQueryString(name, value)}`);
+  };
+
   return (
     <DropdownMenu modal={false}>
       <DropdownMenuTrigger asChild>
@@ -20,13 +44,17 @@ export default function QuestionSortDropdown({ sortValue }: { sortValue: string 
           asChild
           className={`${!sortValue && 'rounded-md shadow-md bg-orange-100'} w-full flex justify-center`}
         >
-          <Link href="/question-list">최신순</Link>
+          <button type="button" onClick={() => handleSorting({ name: 'sort', value: '' })}>
+            최신순
+          </button>
         </DropdownMenuItem>
         <DropdownMenuItem
           asChild
           className={`${sortValue && 'rounded-md shadow-md bg-orange-100'} w-full flex justify-center`}
         >
-          <Link href="/question-list?sort=answerLen">답변순</Link>
+          <button type="button" onClick={() => handleSorting({ name: 'sort', value: 'answerLen' })}>
+            답변순
+          </button>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
