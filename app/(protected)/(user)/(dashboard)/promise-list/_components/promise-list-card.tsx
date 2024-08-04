d4ef6aa6aa6cbf84a7422795937d57e8458a250d
sj0724 <sj07245@naver.com>
2024-08-04 17:31:50 +0900

    fix: 삭제 확인 모달 적용

--- app/(protected)/(user)/(dashboard)/promise-list/_components/promise-list-card.tsx
+++ app/(protected)/(user)/(dashboard)/promise-list/_components/promise-list-card.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import ImageCard from '@/components/image-card';
-import SmallButton from '@/components/small-button';
 import PromiseStatus from '@/app/(protected)/(user)/(dashboard)/promise-list/_components/promise-status';
 import { RequestWithActivity } from '@/type';
 import { deleteActivityRequest } from '@/app/action/activity-request';
 import { toast } from 'sonner';
 import { useRouter } from 'next/navigation';
 import { formatDateRange } from '@/utils/formatDateRange';
+import DeleteAlertModal from '@/components/delete-alert-modal';
 
 export default function PromiseListCard({ promise }: { promise: RequestWithActivity }) {
   const router = useRouter();
@@ -32,14 +32,9 @@ export default function PromiseListCard({ promise }: { promise: RequestWithActiv
       bottomContent={
         <div className="absolute right-3 bottom-3 z-10">
           {promise.status === 'PENDING' && (
-            <SmallButton
-              color="bg-primary"
-              name="취소"
-              onClick={(e) => {
-                e.preventDefault();
-                cancelPromise();
-              }}
-            />
+            <div onClick={(e) => e.preventDefault()}>
+              <DeleteAlertModal deleteAction={cancelPromise} isButton content="취소" />
+            </div>
           )}
         </div>
       }
