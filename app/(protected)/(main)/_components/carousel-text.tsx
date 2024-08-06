e0ce8bd0048893d16207c2fca2e7016738de27bf
juan0444 <dhwjs5267@gmail.com>
2024-08-06 18:51:18 +0900

    design: 캐러셀 디자인 수정

--- app/(protected)/(main)/_components/carousel-text.tsx
+++ app/(protected)/(main)/_components/carousel-text.tsx
@@ -1,22 +1,15 @@
-import { Heart } from 'lucide-react';
-
 interface Props {
   title: string;
-  likes: number;
 }
 
-export default function CarouselText({ title, likes }: Props) {
+export default function CarouselText({ title }: Props) {
   return (
-    <div className="absolute bottom-0 flex justify-between w-full p-3 pt-10 text-white shadow-inner bg-gradient-to-t from-black to-transparent">
-      <div>
+    <div className="absolute bottom-0 flex flex-col justify-end w-full h-full p-3 text-white border-0 rounded-lg shadow-inner bg-gradient-to-t from-black via-transparent to-transparent">
+      <div className="absolute left-3 bottom-7">
         <p className="text-xl font-bold">
-          <span className="text-3xl">길라</span> TOP5
+          <span className="text-2xl">길라</span> TOP5
         </p>
-        <p>{title}</p>
-      </div>
-      <div className="flex items-end gap-2">
-        <Heart className="text-red" />
-        <p>{likes}</p>
+        <p className="text-sm">{title}</p>
       </div>
     </div>
   );
