74bb470b152799ca1fdd793136ddf33273f790ce
sj0724 <sj07245@naver.com>
2024-08-06 11:43:13 +0900

    fix: 폼 스타일 수정

--- app/(protected)/(main)/question-list/_components/qusetion-tip-popover.tsx
+++ app/(protected)/(main)/question-list/_components/qusetion-tip-popover.tsx
@@ -7,13 +7,13 @@ export default function QuestionTipPopOver() {
       <PopoverTrigger className="absolute right-3 top-3">
         <Lightbulb className="w-5 h-5" />
       </PopoverTrigger>
-      <PopoverContent className="bg-white">
-        <p className="text-sm font-semibold text-center">
+      <PopoverContent className="bg-white w-fit right-4">
+        <p className="text-sm font-semibold">
           원하는 정보를 바로 질문해보세요!
           <br /> 길라들이 빠르게 길을 알려줄거에요!
         </p>
-        <p className="text-xs font-semibold text-center">수정과 삭제는 대시보드에서 가능합니다.</p>
-        <p className="text-xs text-center">
+        <p className="text-xs font-semibold">수정과 삭제는 대시보드에서 가능합니다.</p>
+        <p className="text-xs">
           ※ 주의할점: 답변이 등록된 이후에는
           <br /> 수정 및 삭제가 불가능합니다.
         </p>
