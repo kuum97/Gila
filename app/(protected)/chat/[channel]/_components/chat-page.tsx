fb063857c2f1ab656f08fd83057e810cdc137be1
sj0724 <sj07245@naver.com>
2024-09-06 14:14:26 +0900

    fix: approve안된 유저는 대시보드로 이동

--- app/(protected)/chat/[channel]/_components/chat-page.tsx
+++ app/(protected)/chat/[channel]/_components/chat-page.tsx
@@ -1,8 +1,12 @@
+/* eslint-disable no-plusplus */
+
 'use client';
 
 import { Realtime } from 'ably';
 import { AblyProvider, ChannelProvider } from 'ably/react';
 import { RequestWithReqUser, User } from '@/type';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 import Chat from './chat';
 
 interface Props {
@@ -14,11 +18,29 @@ interface Props {
 }
 
 export default function ChatPage({ channel, user, activityTitle, member, owner }: Props) {
+  const router = useRouter();
   const client = new Realtime({
     key: process.env.NEXT_PUBLIC_ABLY_KEY,
     clientId: user.id,
   });
   const channelName = `chat:${channel}`;
+
+  useEffect(() => {
+    let acceptUser = false;
+    for (let i = 0; member.length > i; i++) {
+      if (member[i].requestUserId === user.id) {
+        acceptUser = true;
+        break;
+      }
+    }
+    if (user.id === owner.id) {
+      acceptUser = true;
+    }
+    if (!acceptUser) {
+      alert('접근 권한이 없는 채팅입니다. 채팅리스트로 이동해주세요.');
+      router.replace('/dashboard/my-chat');
+    }
+  }, [member, owner, router, user]);
 
   return (
     <AblyProvider client={client}>
