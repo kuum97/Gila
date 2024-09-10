e24026ca052f28cefb14496b78ae01975937fecc
sj0724 <sj07245@naver.com>
2024-09-10 09:17:28 +0900

    fix: 활동 리스트 suspense, loading제거

--- app/(protected)/(main)/activity-list/page.tsx
+++ app/(protected)/(main)/activity-list/page.tsx
@@ -1,9 +1,7 @@
-import { Suspense } from 'react';
 import { Sort } from '@/type';
 import { getActivities } from '@/app/data/activity';
 import MainCarousel from '@/app/(protected)/(main)/_components/main-carousel';
 import ActivityContainer from '@/app/(protected)/(main)/activity-list/_components/activity-container';
-import Loading from '@/app/(protected)/(main)/activity-list/loading';
 import Link from 'next/link';
 import PlusDiv from '@/components/common/plus-div';
 
@@ -16,23 +14,21 @@ export default async function Page({
 
   return (
     <main className="relative">
-      <Suspense fallback={<Loading />}>
-        <div className="relative pb-8 bg-white shadow-inner border-y-2 z-20">
-          <h1 className="px-4 pt-4 text-xl font-semibold">현재 주목받는 길라들</h1>
-          <MainCarousel />
-        </div>
-        <ActivityContainer
-          activities={activities}
-          cursorId={cursorId}
-          sort={sort}
-          location={location}
-        />
-        <div className="fixed w-8 bottom-24 right-[20px] z-50 tall:right-[calc(50vw-380px)]">
-          <Link href="/dashboard/my-activity/create">
-            <PlusDiv />
-          </Link>
-        </div>
-      </Suspense>
+      <div className="relative pb-8 bg-white shadow-inner border-y-2 z-20">
+        <h1 className="px-4 pt-4 text-xl font-semibold">현재 주목받는 길라들</h1>
+        <MainCarousel />
+      </div>
+      <ActivityContainer
+        activities={activities}
+        cursorId={cursorId}
+        sort={sort}
+        location={location}
+      />
+      <div className="fixed w-8 bottom-24 right-[20px] z-50 tall:right-[calc(50vw-380px)]">
+        <Link href="/dashboard/my-activity/create">
+          <PlusDiv />
+        </Link>
+      </div>
     </main>
   );
 }
