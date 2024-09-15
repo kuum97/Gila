7f9d45c8ddba4456caa55cd7edd493bb65251a65
sj0724 <sj07245@naver.com>
2024-09-15 16:40:02 +0900

    feat: activity 리스트 suspense적용

--- app/(protected)/(main)/activity-list/page.tsx
+++ app/(protected)/(main)/activity-list/page.tsx
@@ -5,6 +5,8 @@ import ActivityContainer from '@/app/(protected)/(main)/activity-list/_component
 import Link from 'next/link';
 import PlusDiv from '@/components/common/plus-div';
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+import { Suspense } from 'react';
+import ActivityListSkeleton from './_components/activity-list-skeleton';
 
 export default async function Page({
   searchParams: { sort, location },
@@ -19,24 +21,26 @@ export default async function Page({
   });
 
   return (
-    <main className="relative">
-      <div className="relative pb-8 bg-white shadow-inner border-y-2 z-20">
-        <h1 className="px-4 pt-4 text-xl font-semibold">현재 주목받는 길라들</h1>
-        <MainCarousel />
-      </div>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <ActivityContainer
-          activities={activities}
-          cursorId={cursorId}
-          sort={sort}
-          location={location}
-        />
-      </HydrationBoundary>
-      <div className="fixed w-8 bottom-24 right-[20px] z-50 tall:right-[calc(50vw-380px)]">
-        <Link href="/dashboard/my-activity/create">
-          <PlusDiv />
-        </Link>
-      </div>
-    </main>
+    <Suspense fallback={<ActivityListSkeleton />}>
+      <main className="relative">
+        <div className="relative pb-8 bg-white shadow-inner border-y-2 z-20">
+          <h1 className="px-4 pt-4 text-xl font-semibold">현재 주목받는 길라들</h1>
+          <MainCarousel />
+        </div>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <ActivityContainer
+            activities={activities}
+            cursorId={cursorId}
+            sort={sort}
+            location={location}
+          />
+        </HydrationBoundary>
+        <div className="fixed w-8 bottom-24 right-[20px] z-50 tall:right-[calc(50vw-380px)]">
+          <Link href="/dashboard/my-activity/create">
+            <PlusDiv />
+          </Link>
+        </div>
+      </main>
+    </Suspense>
   );
 }
